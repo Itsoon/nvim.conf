63c4aa36b0c70792a3f715591ed1eeca7ec91982
Itsoon <91259186+Itsoon@users.noreply.github.com>
2024-08-19 08:11:25 +0200

    update init.lua for neovide

--- init.lua
+++ init.lua
@@ -2,64 +2,47 @@ require("config.lazy")
 
 vim.opt.clipboard:append("unnamedplus")
 
+-- Default options:
+require("gruvbox").setup({
+	terminal_colors = true, -- add neovim terminal colors
+	undercurl = true,
+	underline = true,
+	bold = true,
+	italic = {
+		strings = true,
+		emphasis = true,
+		comments = true,
+		operators = false,
+		folds = true,
+	},
+	strikethrough = true,
+	invert_selection = false,
+	invert_signs = false,
+	invert_tabline = false,
+	invert_intend_guides = false,
+	inverse = true, -- invert background for search, diffs, statuslines and errors
+	contrast = "", -- can be "hard", "soft" or empty string
+	palette_overrides = {},
+	overrides = {},
+	dim_inactive = false,
+	transparent_mode = true,
+})
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+
 if vim.g.neovide then
-	require("gruvbox").setup({
-		terminal_colors = true,
-		undercurl = true,
-		underline = true,
-		bold = true,
-		italic = {
-			strings = true,
-			emphasis = true,
-			comments = true,
-			operators = false,
-			folds = true,
-		},
-		strikethrough = true,
-		invert_selection = false,
-		invert_signs = false,
-		invert_tabline = false,
-		invert_intend_guides = false,
-		inverse = true,
-		contrast = "", -- can be "hard", "soft" or empty string
-		palette_overrides = {
-			dark0 = "#300a24", -- default background color
-			dark1 = "#3D1E33",
-		},
-		overrides = {},
-		dim_inactive = false,
-		transparent_mode = false,
-	})
+	-- Helper function for transparency formatting
+	local alpha = function()
+		return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+	end
+	-- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+	vim.g.neovide_transparency = 0.8
+	vim.g.transparency = 0.8
+	vim.g.neovide_background_color = "#090909" .. alpha()
 
-	vim.o.background = "dark" -- dark or light
-
-	vim.cmd([[colorscheme gruvbox]])
-else
-	-- Default options:
-	require("gruvbox").setup({
-		terminal_colors = true, -- add neovim terminal colors
-		undercurl = true,
-		underline = true,
-		bold = true,
-		italic = {
-			strings = true,
-			emphasis = true,
-			comments = true,
-			operators = false,
-			folds = true,
-		},
-		strikethrough = true,
-		invert_selection = false,
-		invert_signs = false,
-		invert_tabline = false,
-		invert_intend_guides = false,
-		inverse = true, -- invert background for search, diffs, statuslines and errors
-		contrast = "", -- can be "hard", "soft" or empty string
-		palette_overrides = {},
-		overrides = {},
-		dim_inactive = false,
-		transparent_mode = true,
-	})
-	vim.o.background = "dark" -- or "light" for light mode
-	vim.cmd([[colorscheme gruvbox]])
+	-- padding
+	vim.g.neovide_padding_top = 0
+	vim.g.neovide_padding_bottom = 0
+	vim.g.neovide_padding_right = 0
+	vim.g.neovide_padding_left = 0
 end
